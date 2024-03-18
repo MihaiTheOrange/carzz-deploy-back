14d62dbde8947a6cad31ecb9471a6b4f83d98614
SchmidtJosef <schmidtjosefphilipp@gmail.com>
2024-03-18 22:57:50 +0200

    commit8

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 from typing import List
-from uuid import uuid4
+from uuid import UUID
 from fastapi import FastAPI
 from models import Gender, Role, User
 
 app = FastAPI()
 
 db: List[User] = [
-    User(id=uuid4(), first_name="Jamila", last_name="Ahmed", gender=Gender.female, roles=[Role.student]),
-    User(id=uuid4(), first_name="Alex",last_name="Josenes",gender=Gender.male,roles=[Role.admin, Role.user])
+    User(id=UUID("eea55460-5d3c-4679-a78c-b53d764f6b35"),
+         first_name="Jamila",
+         last_name="Ahmed",
+         gender=Gender.female,
+         roles=[Role.student]),
+    User(id=UUID("eb55ddeb-efd6-41f8-a133-cf80a20c7a84"),
+         first_name="Alex",
+         last_name="Jones",
+         gender=Gender.male,
+         roles=[Role.admin, Role.user])
 ]
+
 
 @app.get("/")
 async def root():
     return {"Hello": "Mondo"}
 
+
 @app.get("/api/v1/users")
 async def fetch_users():
-    return db;
+    return db
+
+
+@app.post("/api/v1/users")
+async def register_user(user: User):
+    db.append(user)
+    return {"id": user.id}
