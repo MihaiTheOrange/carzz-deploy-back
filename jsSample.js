18379b93ba3d1c1998ac71947507d6d7d6008a26
SchmidtJosef <schmidtjosefphilipp@gmail.com>
2024-03-18 20:06:03 +0200

    commit 3

--- jsSample.js
+++
@@ -1,6 +0,0 @@
-console.log("-----------------");
-console.log("Hello! My name is Josef!");
-console.log("Today is a new day!");
-console.log("Rise and Shine!");
-console.log("Ready for a new day!");
-console.log("---------------------");
