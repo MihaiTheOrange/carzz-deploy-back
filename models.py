a7317d49fe75e48d7b2448add481d63bfd0c554d
SchmidtJosef <schmidtjosefphilipp@gmail.com>
2024-03-26 16:44:53 +0200

    Announcements model

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
@@ -13,3 +13,15 @@ class Users(Base):
     full_name = Column(String, unique=True)
     email = Column(String, unique=True)
     role = Column(String)
+
+
+class Announcements(Base):
+    __tablename__ = "announcements"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, index=True)
+    description = Column(String)
+    price = Column(Float)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    user = relationship("Users", back_populates="announcements")
